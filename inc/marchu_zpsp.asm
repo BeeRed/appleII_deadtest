c6b775f7fc6a8be7688c283d19c4fac4cd192de1
Thondons <thomas@wenzig.de>
2026-07-22 22:32:54 +0200

    Detailed error messages at zpsp test

--- inc/marchu_zpsp.asm
+++ inc/marchu_zpsp.asm
@@ -16,22 +16,24 @@
 ; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 .scope	ZPSP
-
+; print "TEST ZERO PAGE"
 		inline_print zp_msg, TXTLINE21+((40-(zp_end-zp_msg-1))/2)
-
 start:	
-		LDX #(tst_tbl_end-tst_tbl-1)	; initialize the pointer to the table of values
+		LDX #(tst_tbl_end-tst_tbl-1)	; initialize the pointer to the table of values - start from end of the table
 
-; step 0; up - w0 - write the test value
+; step 0; up - w0
+; write the test value to zero page and stack
 marchU:	
-		LDA tst_tbl,X	; get the test value into A
-		TXS				; save the index of the test value into SP
-		TAX				; save the test value into X
-		
+		inline_print zp_step0, TXTLINE22+16 ; print "STEP 0: ", A and Y will be used 
+		LDA tst_tbl,X	; get the test value from tst_tbl into A
+		TXS				; save the pointer index of the test value into SP
+		TAX				; move the test value into X
+		inline_hex_x TXTLINE22+24 ; print test value in X to screen, A and Y will be used by inline routine
+		TXA				; restore test value back to A
 		LDY #$27		; write value at bottom of screen
-	:	STA $07D0,Y
+loopy:	STA TXTLINE24,Y
 		DEY
-		BPL :-
+		BPL loopy
 
 		LDY #$00
 marchU0:
@@ -40,9 +42,11 @@ marchU0:
 		INY				; count up
 		BNE marchU0		; repeat until Y overflows back to zero
 
-		; STY $00			; simulate an error for testing
 ; step 1; up - r0,w1,r1,w0
-; A contains test value
+; A and X contains test value
+		inline_print zp_step1, TXTLINE22+16 ; print "STEP 1: ", A and Y will be used 
+		TXA  			; restore test value to A
+		LDY #$00
 marchU1:EOR $00,Y		; r0 - read and compare with test value (by XOR'ing with accumulator)
 		BNE zp_bad		; if bits differ, location is bad
 		TXA				; get the test value
@@ -66,11 +70,11 @@ marchU1:EOR $00,Y		; r0 - read and compare with test value (by XOR'ing with accu
 
 ; 100ms delay for finding bit rot
 marchU1delay:
-		inline_delay_cycles_ay 10000
-		
+		inline_delay_cycles_ay 10000; A and Y will be used
+		inline_print zp_step2, TXTLINE22+16; print "STEP 2: ", A and Y will be used 
 		LDY #$00		; reset Y to 0
 ; step 2; up - r0,w1
-; A contains test value from prev step
+; X contains test value from prev step
 marchU2:TXA				; recover test value
 		EOR $00,Y		; r0 - read and compare with test value (by XOR'ing with accumulator)
 		BNE zp_bad		; if bits differ, location is bad
@@ -86,20 +90,22 @@ marchU2:TXA				; recover test value
 
 ; 100ms delay for finding bit rot
 marchU2delay:
-		inline_delay_cycles_ay 10000
+		inline_delay_cycles_ay 10000; A and Y will be used
+		inline_print zp_step3, TXTLINE22+16; print "STEP 3: ", A and Y will be used 
 		JMP continue
 
-zp_bad:	JMP zp_error
+zp_bad: 	JMP zp_error; 
 
 continue:
+; step 3; down - r1,w0,r0,w1
+; X contains test value from prev step		
 		LDY #$FF		; reset Y to $FF and count down
 		TXA				; recover test value
 		EOR #$FF		; invert
-; step 3; down - r1,w0,r0,w1
 marchU3:EOR $00,Y		; r1 - read and compare with inverted test value (by XOR'ing with accumulator)
 		BNE zp_bad		; if bits differ, location is bad
 		TXA				; get the test value
-		EOR #$FF
+		EOR #$FF		; invert
 		EOR $0100,Y		; r1s - also stack page
 		BNE zp_bad		; if bits differ, location is bad
 		TXA				; get the test value
@@ -119,7 +125,11 @@ marchU3:EOR $00,Y		; r1 - read and compare with inverted test value (by XOR'ing 
 		BNE marchU3		; repeat until Y overflows back to FF
 
 ; step 4; down - r1,w0
-; A contains the inverted test value from prev step
+; X contains test value from prev step		
+		inline_print zp_step4, TXTLINE22+16; print "STEP 4: ", Akku and Y will be used 
+		TXA				; get the test value
+		EOR #$FF		; invert
+		LDY #$00
 marchU4:EOR $00,Y		; r1 - read and compare with inverted test value (by XOR'ing with accumulator)
 		BNE zp_bad		; if bits differ, location is bad
 		TXA				; get the test value
@@ -153,44 +163,47 @@ marchup:
 
 
 .proc zp_error
-		TAX				; bat bit mask is in A, save it to X
-		TXS  			; then save it in the SP
+		TAX						; move EOR-result to X
+		TXS  					; then save it in the SP
+		TYA						; move adresse from Y to A
+		TAX						; and from A to X
+		inline_hex_x 	TXTLINE23+14  ; print adress in X to screen;
+		STA TXTSET				; text mode
+		STA MIXSET				; mixed mode on
+		STA LOWSCR				; page 2 off
 
-		STA TXTSET		; text mode
-		sta MIXSET		; mixed mode on
-		STA LOWSCR		; page 2 off
-		inline_cls
-		inline_print bad_msg, $0750
+		inline_print bad_msg, TXTLINE23; print bad_msg to screen, A and Y will be used
+		inline_print zp_pattern_msg, TXTLINE23+18; print zp_pattern_msg to screen, A and Y will be used
 
-		TSX				; retrieve the test value
-		TXA
+		TSX						; get EOR-result from SP
+		TXA						; and move it to A
 		LDY #0
 	print_bit:
-		asl				; get top bit into carry flag
-		tax				; save the current value
+		asl						; get top bit into carry flag
+		tax						; save the current value
 		lda #'0'|$80
-		adc #0			; increment by one if we had a carry
-		sta $075A,Y		; print bit to screen
+		adc #0					; increment by one if we had a carry
+		sta TXTLINE23+30,Y		; print bit to screen
 		txa
 		iny
 		cpy #8
-		bne print_bit	; repeat 8 times
+		bne print_bit			; repeat 8 times
 
 	; find the bit to beep out
-		tsx				; get the bad bit mask back into A
+		tsx						; get the bad bit mask back into A
 		txa
-		LDX #1			; count up
+		LDX #1			
 	chkbit:	
-		LSR				; move lowest bit into carry
-		BCS start_beeping	; bit set, display it
-		inx				; count down
+		LSR						; move lowest bit into carry
+		BCS start_beeping		; bit set, display it
+		inx						; count up
 		cpx #$09
-		bne chkbit	; test next bit
-	wha:JMP wha			; only get here if there was no bad bit
+		bne chkbit				; test next bit
+	wha:JMP wha					; only get here if there was no bad bit
 
 	; now X contains the index of the bit, starting at 1
 	start_beeping:
-		txs				; save the bit index of the top set bit into SP
+		txs						; save the bit index of the top set bit into SP
 	beeploop:
 		lda #1
 	type_beep:					; beep an annoying chirp to indicate page err
@@ -199,15 +212,15 @@ marchup:
 		sbc #1
 		bpl type_beep
 
-		tsx					; fetch the bit number
+		tsx						; fetch the bit number
 		txa
 	bit_beep:
 		tax
 		inline_delay_cycles_ay 400000
 		txa
-		sta TXTCLR 			; turn on graphics
+		sta TXTCLR 				; turn on graphics
 		inline_beep_xy $FF, $80
-		sta TXTSET			; text mode
+		sta TXTSET				; text mode
 		sec
 		sbc #1
 		bne bit_beep
@@ -222,7 +235,7 @@ marchup:
 		JMP beeploop
 .endproc
 
-bad_msg:.apple2sz "ZP/SP ERR"
+bad_msg:.apple2sz "ZP/SP ERR AT $"
 	bad_msg_len = * - bad_msg
 
 zp_good:
@@ -272,14 +285,13 @@ zp_good:
 .endproc
 
 .proc page_error
-		; TAX				; bat bit mask is in A, save it to X
-		TXS  			; then save it in the SP
+		TXS  			; bad bit mask is in X, save it in the SP
 
 		STA TXTSET		; text mode
 		sta MIXSET		; mixed mode on
 		STA LOWSCR		; page 2 off
 		inline_cls
-		inline_print bad_page_msg, $0750
+		inline_print bad_page_msg, TXTLINE23
 
 		TSX				; retrieve the test value
 		TXA
@@ -289,7 +301,7 @@ zp_good:
 		tax				; save the current value
 		lda #'0'|$80
 		adc #0			; increment by one if we had a carry
-		sta $0759,Y		; print bit to screen
+		sta $0764,Y		; print bit to screen
 		txa
 		iny
 		cpy #8
